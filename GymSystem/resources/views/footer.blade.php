b4ddacab1b6808420c793181044948870e256b6d
Artiom-Botnari <botnariartiom97@gmail.com>
2021-06-23 19:49:27 +0300

    Create footer view page

---
+++ GymSystem/resources/views/footer.blade.php
@@ -0,0 +1,38 @@
+@section('footer')
+<!--Footer-->
+<footer class="container mt-5">
+		 <div class="row">
+			 <div class="col-lg-6 ">
+				 <div class="social_links">
+					 <a href="https://www.facebook.com/" target="_blank">
+						 <img class="bg-dark rounded-circle p-1 social_image_size " src="../img/facebook.png" alt="facebook social image">
+					 </a>
+					 <a href="https://www.instagram.com/" target="_blank">
+						 <img class="bg-dark rounded-circle p-1  social_image_size" src="../img/instagram.png" alt="instagram social image">
+					 </a>
+					 <a href="https://www.youtube.com/" target="_blank">
+						 <img class="bg-dark rounded-circle p-1  social_image_size" src="../img/youtube.png" alt="youtube social image">
+					 </a>
+					 <a href="https://www.pinterest.com/" target="_blank">
+						<img class="bg-dark rounded-circle p-1  social_image_size" src="../img/printerest.png" alt="printerest social image">
+					</a>
+				 </div>
+				 <div class="mt-3">
+					 <p>Copying site materials is prohibited</p>
+					 <a href="{{ route('terms_and_conditions') }}">Terms and conditions</a>
+					 <p>© 2021 GYM SYSTEM. All rights reserved.</p>
+				 </div>
+			 </div>
+			 <div class="col-lg-6">
+				 <div>
+					 <p>Our location:</p>
+					 <p>Moldova.Chisinau.</p>
+				 </div>
+				 <div>
+					 <p>Phone: 022 00 00 00</p>
+					 <p>Email: examplegymsystem@gmail.com</p>
+				 </div>
+			 </div>
+		 </div>
+	 </footer>
+	 <!--/.Footer-->
