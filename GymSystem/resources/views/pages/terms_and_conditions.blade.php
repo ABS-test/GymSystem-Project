72a6ba43dc1cefb834eb3ed3d76f272c84d9b497
Artiom-Botnari <botnariartiom97@gmail.com>
2021-06-23 19:46:49 +0300

    Create terms and conditions  page

---
+++ GymSystem/resources/views/pages/terms_and_conditions.blade.php
@@ -0,0 +1,60 @@
+@extends('layout')
+
+@section('title') Terms and Conditions @endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Terms and conditions</h2>
+                <div>
+                    <h4>General</h4>
+                    <p>This document states the terms and conditions (hereafter “Terms”) under which 
+                        individuals and organizations (hereafter “Customer”) may use DevelopmentAid’s services (hereafter “the Service”). 
+                        The purpose of this document is to define the procedures regarding access and use of the Service.
+                    </p>
+                    <p>These terms constitute a binding agreement between the Customer and the service provider -DEVAID Limited (hereafter “the Company”), 
+                        and are deemed accepted by the Customer each time that the Customer uses or accesses the Service. 
+                        These Website Standard Terms and Conditions written on this webpage shall manage Customer’s use of the Service.
+                    </p>
+                    <p>
+                        The Company reserves the right to amend the terms at any point in time by posting an updated version to this web page. 
+                        Customer should visit this page periodically to review the most current terms because these are binding to the Customer.
+                        Minors or people below 18 years old are not allowed to use the Service.
+                    </p>
+                </div>
+                <div>
+                    <h4>Accessing the service</h4>
+                    <p>The rights to access and use of the Service are exclusive.</p>
+                    <p>The customer is not allowed to transfer, sell, or assign any rights and obligations to the service to any third party.</p>
+                    <p>Membership is granted to the specific office of the organization in the country of origin and can not be shared with their 
+                        offices of the same organization in other countries.
+                    </p>
+                    <p>To limit the risk of piracy and theft, the Company reserves the right to limit access to parts or whole of the Service. 
+                        This includes, but is not limited to the possibility of introducing daily view and download limits.
+                    </p>
+                    <p>Access to paid parts of the Service is granted only upon receipt of payment from Customer.</p>
+                </div>
+                <div>
+                    <h4>Prices and payment</h4>
+                    <p>The prices can be subject to change at any time with prior notification via email or via the website. 
+                        New prices can only be applied to an existing Customer at the moment of re-subscription or prolongation.
+                    </p>
+                    <p>The Company bills at the beginning of every renewal period to the bank account or credit card indicated 
+                        during the registration process (or changed after initial registration).
+                        Billing takes place unless cancellation occurs at least 7 days before the renewal period begins.
+                    </p>
+                    <p>Customer should indicate any discrepancies in the invoice to the Company within sixty (60) calendar days from the date of issue. 
+                        Failure to indicate discrepancies within this period, shall lead to the payment being considered correct.
+                    </p>
+                    <p>All payments are to be made in cleared funds, without any deduction or off-set and free and clear of and without deduction 
+                        for or on account of any taxes, levies, imports, duties, charges, fees and withholdings of any nature now or hereafter imposed by 
+                        any governmental, fiscal or other authority save as required by law. If a Customer is compelled to make any such deduction, 
+                        it will pay to the Company such additional amounts as are necessary to ensure receipt by the Company of the full amount which the 
+                        Company would have received but for the deduction.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
